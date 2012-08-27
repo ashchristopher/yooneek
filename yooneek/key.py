c880148791134861ca10cf110a7edfae1121ecd9
Ash Christopher <ash.christopher@gmail.com>
2012-08-26 21:07:28 -0400

    Added key generating method

--- yooneek/key.py
+++ yooneek/key.py
@@ -1,15 +1,4 @@
-from functools import wraps
-
-
-def sequence(fn):
-
-    @wraps(fn)
-    def wrapper(self):
-        self.sequence += 1
-        if self.sequence > IDGenerator.MAX_SEQ:
-            self.sequence = 0
-        return fn(self)
-    return wrapper
+import time
 
 
 class IDGenerator(object):
@@ -17,11 +6,34 @@ class IDGenerator(object):
     MAX_SEQ = 0b11111111111
 
     def __init__(self):
-        self.host = None
+        self.host = 1
         self.sequence = 0
+        self.timestamp = 0
+    
+    def _prime_timestamp(self):
+        # not all versions of Python support getting milli-seconds, so we only rely on seconds
+        timestamp = int(time.time())
         
-    @sequence
+        if timestamp < self.timestamp:
+            raise Exception
+        
+        self.timestamp = timestamp
+
+    def _prime_sequence(self):
+        seq = self.sequence + 1
+        if seq > IDGenerator.MAX_SEQ:
+            self.sequence = 0
+        else:
+            self.sequence = seq
+
     def generate_key(self):
-        return self.sequence
+        self._prime_sequence()
+        self._prime_timestamp()
+    
+        key = self.timestamp << (64 - 41)
+        key |= self.host << (64 - 41 - 11)
+        key |= self.sequence
+
+        return key
 
 generator = IDGenerator()
